b7efb55c3844b30edb03f058e60676c07b4cf7ad
Harsh530511 <harshking5305@gmail.com>
2026-04-29 16:11:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    // This tells the underlying Nitro server to use the Vercel adapter
-    // instead of the default Cloudflare one.
-    nitro: {
-      preset: 'vercel'
+    build: {
+      outDir: 'dist',
     }
   }
 });
