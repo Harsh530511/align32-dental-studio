fd3cf9346f1e3ee8f890b2a93bc0d832aa53adcb
Harsh530511 <harshking5305@gmail.com>
2026-04-29 16:29:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    build: {
-      outDir: 'dist',
+    nitro: {
+      preset: 'vercel' 
     }
   }
 });
